3810dc8ce81ac3cbb73558d9015649fa60b56fa3
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-08-21 19:19:30 +0100

    🎉 Initial scaffolding

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Loom',
+      favicon: '/favicon.svg',
+      logo: {
+        src: './src/assets/loom.svg',
+        alt: 'Loom Logo',
+        replacesTitle: false,
+      },
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/loomkit/starter' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
